6df07c9d9729a80f2a5e07b55ce26eb99b45331f
huda <sharawyh43@gmail.com>
2025-12-04 15:02:23 -0800

    Fix auth widgets and auth screen, add Google Sign-In

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:domi_cafe/features/profile/screens/profile_screen.dart';
+
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,9 +8,21 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
+      appBar: AppBar(
+        title: const Text("Home"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.person),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => ProfileScreen()), // بدون const
+              );
+            },
+          ),
+        ],
       ),
+      body: const Center(child: Text("Home Content")),
     );
   }
 }
